8143b72c967ad7e79d5379d0aed6a6a6a21bde6f
ajaberi <adnan.ajaberi@gmail.com>
2025-02-18 14:58:10 +0300

    done wiht bushes

--- 16-Haunted-house/src/script.js
+++ 16-Haunted-house/src/script.js
@@ -71,6 +71,14 @@ roofColorTexture.wrapS = THREE.RepeatWrapping
 roofColorTexture.wraps = THREE.RepeatWrapping
 roofARMTexture.wrapS = THREE.RepeatWrapping
 
+// Bush
+const bushColorTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.jpg')
+const bushARMTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.jpg')
+const bushNormalTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_nor_1k.jpg')
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace
+bushARMTexture.colorSpace = THREE.SRGBColorSpace
+
 /**
  * House
  */
@@ -142,23 +150,34 @@ house.add(door)
 
 // Bushes
 const bushGeometry = new THREE.SphereGeometry(1,16,16)
-const bushMaterial = new THREE.MeshStandardMaterial()
+const bushMaterial = new THREE.MeshStandardMaterial({
+    color: '#ccffcc',
+    map: bushColorTexture,
+    aoMap: bushARMTexture,
+    roughness: bushARMTexture,
+    metalnessMap: bushARMTexture,
+    normalMap: bushNormalTexture,
+})
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush1.scale.set(0.5, 0.5, 0.5)
 bush1.position.set(0.8, 0.1, 2.1)
+bush1.rotation.x = -0.75
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush2.scale.set(0.25, 0.25, 0.25)
 bush2.position.set(1.4, 0.1, 2.1)
+bush2.rotation.x = -0.75
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush3.scale.set(0.4, 0.4, 0.4)
 bush3.position.set(-0.8, 0.1, 2.2)
+bush3.rotation.x = -0.75
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush4.scale.set(0.15, 0.15, 0.15)
 bush4.position.set(-1, 0.05, 2.6)
+bush4.rotation.x = -0.75
 
 house.add(bush1, bush2, bush3, bush4)
 
