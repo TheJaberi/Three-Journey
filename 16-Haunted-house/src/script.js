e58ce1165cd43291e31439a70e61935922f11cfb
ajaberi <adnan.ajaberi@gmail.com>
2025-02-18 16:11:22 +0300

    ghosts done

--- 16-Haunted-house/src/script.js
+++ 16-Haunted-house/src/script.js
@@ -53,7 +53,7 @@ const wallARMTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/cast
 const wallNormalTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_1k.jpg')
 
 wallColorTexture.colorSpace = THREE.SRGBColorSpace
-wallARMTexture.colorSpace = THREE.SRGBColorSpace
+// wallARMTexture.colorSpace = THREE.SRGBColorSpace
 
 // Roof
 const roofColorTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg')
@@ -76,7 +76,7 @@ const bushARMTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves
 const bushNormalTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_nor_1k.jpg')
 
 bushColorTexture.colorSpace = THREE.SRGBColorSpace
-bushARMTexture.colorSpace = THREE.SRGBColorSpace
+// bushARMTexture.colorSpace = THREE.SRGBColorSpace
 
 bushColorTexture.repeat.set(2,1)
 bushARMTexture.repeat.set(2,1)
@@ -272,6 +272,24 @@ const directionalLight = new THREE.DirectionalLight('#86cdff', 1)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
 
+// Door Light
+const doorLight = new THREE.PointLight('#ff7d46', 5)
+doorLight.position.set(0, 2.2, 2.5)
+house.add(doorLight)
+
+// const pointLightCameraHelper = new THREE.PointLightHelper(doorLight, 0.2)
+// pointLightCameraHelper.visible = true
+// house.add(pointLightCameraHelper)
+
+
+/**
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight('#8800ff', 6)
+const ghost2 = new THREE.PointLight('#ff0088', 6)
+const ghost3 = new THREE.PointLight('#ff0000', 6)
+scene.add(ghost1, ghost2, ghost3)
+
 /**
  * Sizes
  */
@@ -328,6 +346,23 @@ const tick = () =>
     // Timer
     timer.update()
     const elapsedTime = timer.getElapsed()
+
+    // Ghosts
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(ghost1Angle) * Math.sin(ghost1Angle * 2.34) * Math.sin(ghost1Angle * 3.45)
+
+    const ghost2Angle = - elapsedTime * 0.38
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(ghost2Angle) * Math.sin(ghost2Angle * 2.34) * Math.sin(ghost2Angle * 3.45)
+
+
+    const ghost3Angle = elapsedTime * 0.23
+    ghost3.position.x = Math.cos(ghost3Angle) * 6
+    ghost3.position.z = Math.sin(ghost3Angle) * 6
+    ghost3.position.y = Math.sin(ghost3Angle) * Math.sin(ghost3Angle * 2.34) * Math.sin(ghost3Angle * 3.45)
 
     // Update controls
     controls.update()
