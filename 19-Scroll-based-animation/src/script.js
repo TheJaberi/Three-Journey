65a99342be6cfc8ac61432ebba8f8c7e0d4ecabb
ajaberi <adnan.ajaberi@gmail.com>
2025-02-19 16:46:06 +0300

    added three objects

--- 19-Scroll-based-animation/src/script.js
+++ 19-Scroll-based-animation/src/script.js
@@ -25,11 +25,28 @@ const scene = new THREE.Scene()
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// )
+// scene.add(cube)
+
+/**
+ * Objects
+ */
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    new THREE.MeshBasicMaterial({color:'#ff0000'})
 )
-scene.add(cube)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    new THREE.MeshBasicMaterial({color:'#ff0000'})
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    new THREE.MeshBasicMaterial({color:'#ff0000'})
+)
+scene.add(mesh1, mesh2, mesh3)
 
 /**
  * Sizes
