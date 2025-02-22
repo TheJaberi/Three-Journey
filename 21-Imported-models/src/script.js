44d9cd4bebf98cea7fd5a2d73e3d761ecb267981
ajaberi <adnan.ajaberi@gmail.com>
2025-02-22 18:37:17 +0300

    did animations with mixer, and update the mixer on every tick

--- 21-Imported-models/src/script.js
+++ 21-Imported-models/src/script.js
@@ -25,14 +25,22 @@ dracoLoader.setDecoderPath('/draco/')
 const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
 
+let mixer = null
+
 gltfLoader.load(
-    'models/Duck/glTF-Draco/Duck.gltf',
+    'models/Fox/glTF/Fox.gltf',
     (gltf) =>{
         // const children = [...gltf.scene.children]
         // for(const child of children)
         // {
         //     scene.add(child)
         // }
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer.clipAction(gltf.animations[2])
+
+        action.play()
+
+        gltf.scene.scale.set(0.025, 0.025, 0.025)
         scene.add(gltf.scene)
     }
 )
@@ -127,6 +135,12 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+
+    // Update mixer
+    if(mixer !== null){
+        mixer.update(deltaTime)
+    }
+    
 
     // Update controls
     controls.update()
