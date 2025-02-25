3ca130a7a15de55b9efe16c067a77e7cd8de05c0
ajaberi <adnan.ajaberi@gmail.com>
2025-02-25 12:54:38 +0300

    did it with hdr

--- 24-Environment_maps/src/script.js
+++ 24-Environment_maps/src/script.js
@@ -2,12 +2,14 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { RGBELoader} from 'three/addons/loaders/RGBELoader.js'
 
 /**
  * Loaders
  */
 const gltfLoader = new GLTFLoader()
 const cubeTextureLoader = new THREE.CubeTextureLoader()
+const rgbeLoader = new RGBELoader()
 
 /**
  * Base
@@ -37,17 +39,27 @@ gui.add(scene.backgroundRotation, 'y').min(0).max(Math.PI * 2).step(0.001).name(
 gui.add(scene.environmentRotation, 'y').min(0).max(Math.PI * 2).step(0.001).name('Environment rotation')
 
 // LDR cube texture
-const environmentMap = cubeTextureLoader.load([
-    '/environmentMaps/0/px.png',
-    '/environmentMaps/0/nx.png',
-    '/environmentMaps/0/py.png',
-    '/environmentMaps/0/ny.png',
-    '/environmentMaps/0/pz.png',
-    '/environmentMaps/0/nz.png'
-])
+// const environmentMap = cubeTextureLoader.load([
+//     '/environmentMaps/0/px.png',
+//     '/environmentMaps/0/nx.png',
+//     '/environmentMaps/0/py.png',
+//     '/environmentMaps/0/ny.png',
+//     '/environmentMaps/0/pz.png',
+//     '/environmentMaps/0/nz.png'
+// ])
 
-scene.environment = environmentMap
-scene.background = environmentMap
+// scene.environment = environmentMap
+// scene.background = environmentMap
+
+// HDR (RGBE) equirectangular
+rgbeLoader.load(
+    '/environmentMaps/0/2k.hdr',
+    (environmentMap) => {
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping
+        scene.background = environmentMap
+        scene.environment = environmentMap
+    }
+)
 
 /**
  * Torus Knot
